19eaf08d253dd78bdb9895a32f0fd22af1bd5dce
Martin Klacik <klacikmartin@protonmail.com>
2024-05-02 03:19:44 +0200

    migrations

--- database/migrations/2024_05_02_010820_create_questions.php
+++ database/migrations/2024_05_02_010820_create_questions.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('permission', config('enums.permissions'))->default(config('enums.permissions.USER'));
-            $table->rememberToken();
+            $table->string('question');
+            $table->char('code', 5);
+            $table->foreign('id')->references('id')->on('users')->onDelete('cascade');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('questions');
     }
 };
--- database/migrations/2024_05_02_011341_create_answers.php
+++ database/migrations/2024_05_02_011341_create_answers.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('permission', config('enums.permissions'))->default(config('enums.permissions.USER'));
-            $table->rememberToken();
+            $table->string('answer', 255);
+            $table->foreign('id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('answers');
     }
 };
