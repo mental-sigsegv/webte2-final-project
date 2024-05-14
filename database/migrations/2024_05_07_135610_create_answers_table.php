869524bdb8c2ba68f74351eaaeef643ae961d415
Martin Klacik <klacikmartin@protonmail.com>
2024-05-15 00:58:24 +0200

    migration

--- database/migrations/2024_05_07_135610_create_answers_table.php
+++ database/migrations/2024_05_07_135610_create_answers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('question_code')->constrained('questions');
+            $table->string('answer');
             $table->timestamps();
         });
     }
