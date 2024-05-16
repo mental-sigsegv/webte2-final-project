a69502ce8c6a4e4c99b569a26993e8cb268bf979
slavkoleminh <slavko007997@gmail.com>
2024-05-16 17:50:44 +0200

    navbar and flags

--- resources/views/components/nav/nav.blade.php
+++ resources/views/components/nav/nav.blade.php
@@ -1,32 +1,34 @@
-<div class="mx-10 flex items-baseline space-x-4 space-y-4 mb-3">
+<div class="mx-10 py-5 flex mb-3">
     <x-nav.nav-link href="/" :active="request()->is('/')">{{ __('navbar.home') }}</x-nav.nav-link>
     <x-nav.nav-link href="/qrcode" :active="request()->is('qrcode')">QR Code</x-nav.nav-link>
 
     <x-nav.nav-link href="/manual" :active="request()->is('manual')">{{ __('navbar.manual') }}</x-nav.nav-link>
 
 
-
     @auth
-        <x-nav.nav-link href="/questions" :active="request()->is('questions')">{{ __('navbar.questions') }}</x-nav.nav-link>
-        <x-nav.nav-link href="/question/create" :active="request()->is('question/create')">{{ __('navbar.new-question') }}</x-nav.nav-link>
+        <x-nav.nav-link href="/questions"
+                        :active="request()->is('questions')">{{ __('navbar.questions') }}</x-nav.nav-link>
+        <x-nav.nav-link href="/question/create"
+                        :active="request()->is('question/create')">{{ __('navbar.new-question') }}</x-nav.nav-link>
         @if(auth()->user()->role === 'Admin')
             <x-nav.nav-link href="/admin" :active="request()->is('admin')">Admin</x-nav.nav-link>
         @endif
     @endauth
 
-    <div style="margin-left: auto;">
+    <div style="margin-left: auto;" class="flex">
         @if(!auth()->check())
             <x-nav.nav-link href="/login" :active="request()->is('login')">{{ __('navbar.login') }}</x-nav.nav-link>
-            <x-nav.nav-link href="/register" :active="request()->is('register')">{{ __('navbar.register') }}</x-nav.nav-link>
+            <x-nav.nav-link href="/register"
+                            :active="request()->is('register')">{{ __('navbar.register') }}</x-nav.nav-link>
         @endif
 
         @auth
             <x-nav.nav-link href="/logout" :active="request()->is('logout')">{{ __('navbar.logout') }}</x-nav.nav-link>
-            <x-nav.nav-link href="/reset_password" :active="request()->is('reset_password')">{{ __('navbar.reset-password') }}</x-nav.nav-link>
+            <x-nav.nav-link href="/reset_password"
+                            :active="request()->is('reset_password')">{{ __('navbar.reset-password') }}</x-nav.nav-link>
         @endauth
     </div>
 
-    <div>
-        @include('components.nav.language-switcher')
-    </div>
+    @include('components.nav.language-switcher')
 </div>
+
--- resources/views/livewire/question-option.blade.php
+++ resources/views/livewire/question-option.blade.php
@@ -1,10 +1,12 @@
-<div>
-    <h1>Options</h1>
+<div class="max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg">
+    <h1 class="text-2xl font-semibold mb-4">Options</h1>
     <label for="{{ $id }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option</label>
-    <input wire:model="value" wire:change="updateValue" type="text" id="{{ $id }}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Question" required="">
+    <input wire:model="value" wire:change="updateValue" type="text" id="{{ $id }}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4" placeholder="Option" required="">
 
-    <label for="correct">Correct</label><br>
-    <input type="checkbox" wire:change="updateValue" id="correct" wire:model="correct" value="1">
+    <div class="flex items-center mb-4">
+        <input type="checkbox" wire:change="updateValue" id="correct" wire:model="correct" value="1" class="mr-2">
+        <label for="correct" class="text-sm font-medium text-gray-900 dark:text-white">Correct</label>
+    </div>
 
-    <button type="button" wire:click="remove">Remove</button>
+    <button type="button" wire:click="remove" class="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75">Remove</button>
 </div>
