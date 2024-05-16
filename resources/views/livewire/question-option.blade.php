76b2ae476cdac4cbb3e2041e42a854d3aa3d6aa0
slavkoleminh <slavko007997@gmail.com>
2024-05-16 03:00:28 +0200

    frontend for table in QUESTIONS

--- resources/views/livewire/question-option.blade.php
+++ resources/views/livewire/question-option.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <h1>Option</h1>
+    <h1>Options</h1>
     <label for="{{ $id }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option</label>
     <input wire:model="value" wire:change="updateValue" type="text" id="{{ $id }}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Question" required="">
 
--- resources/views/pages/questions.blade.php
+++ resources/views/pages/questions.blade.php
@@ -1,44 +1,55 @@
 <x-layouts.app>
-    <table id="showQuestions" class="table table-bordered">
-        <thead>
-        <tr>
-            <th>User</th>
-            <th>Code</th>
-            <th>Question</th>
-            <th>Created At</th>
-            <th>Updated At</th>
-            <th>Active</th>
-            <th>Open</th>
-            <th>Subject</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($questionsData as $data)
+    <div class="overflow-x-auto">
+        <table id="showQuestions" class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+            <thead class="bg-gray-800 text-white">
             <tr>
-                <td>{{ $data->name }}</td>
-                <td>{{ $data->code }}</td>
-                <td>{{ $data->question }}</td>
-                <td>{{ $data->created_at }}</td>
-                <td>{{ $data->updated_at }}</td>
-                <td>{{ $data->active == 1 ? 'Yes' : 'No' }}</td>
-                <td>{{ $data->open == 1 ? 'Yes' : 'No'  }}</td>
-                <td>{{ $data->subjectName }}</td>
-                <td>
-                    <form method="POST" action="{{ route('questions.delete', ['questionId' => $data->id]) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">Delete</button>
-                    </form>
-                </td>
-                <td>
-                    <form method="POST" action="{{ route('questions.update', ['questionId' => $data->id]) }}">
-                        @csrf
-                        @method('PATCH')
-                        <button type="submit">Update</button>
-                    </form>
-                </td>
+                <th class="px-4 py-2">User</th>
+                <th class="px-4 py-2">Code</th>
+                <th class="px-4 py-2">Question</th>
+                <th class="px-4 py-2">Created At</th>
+                <th class="px-4 py-2">Updated At</th>
+                <th class="px-4 py-2">Active</th>
+                <th class="px-4 py-2">Open</th>
+                <th class="px-4 py-2">Subject</th>
+                <th class="px-4 py-2">Actions</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach ($questionsData as $data)
+                <tr class="border-b bg-white even:bg-gray-100">
+                    <td class="px-4 py-2">{{ $data->name }}</td>
+                    <td class="px-4 py-2">{{ $data->code }}</td>
+                    <td class="px-4 py-2">{{ $data->question }}</td>
+                    <td class="px-4 py-2">{{ $data->created_at }}</td>
+                    <td class="px-4 py-2">{{ $data->updated_at }}</td>
+                    <td class="px-4 py-2">{{ $data->active == 1 ? 'Yes' : 'No' }}</td>
+                    <td class="px-4 py-2">{{ $data->open == 1 ? 'Yes' : 'No'  }}</td>
+                    <td class="px-4 py-2">{{ $data->subjectName }}</td>
+                    <td class="px-4 py-2 flex space-x-2">
+                        <form method="POST" action="{{ route('questions.delete', ['questionId' => $data->id]) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+
+                            </button>
+                        </form>
+                        <form method="POST" action="{{ route('questions.update', ['questionId' => $data->id]) }}">
+                            @csrf
+                            @method('PATCH')
+                            <button type="submit" class="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-700">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                </svg>
+
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 </x-layouts.app>
