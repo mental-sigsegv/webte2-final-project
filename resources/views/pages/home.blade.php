035aabdfc97f191ec715eeb4ddd4fb106e6d01cc
ErikPoczos <erik.poczos@gmail.com>
2024-05-16 17:37:17 +0200

    Base for viewing question from code

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,12 +16,17 @@
             {{ __('message.welcome') }}
         </p>
 
+
+        @if(session()->has('error'))
+            <p class="text-red-500">{{ session('error') }}</p>
+        @endif
+
         <button onclick="{{ session(['lacel' => 'sk']) }}">
             Change Language EN
         </button>
     </div>
 
-
+    @livewire('code')
     @livewire('wire-elements-modal')
     <button onclick="Livewire.dispatch('openModal', { component: 'modal-test' })">Open modal</button>
 
