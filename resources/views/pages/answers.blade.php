9555a989238bf2e9cffbabb2564df324cad0c231
jandemeter <jan.demeter1010@gmail.com>
2024-05-17 17:38:32 +0200

    add showing results for both types of questions

--- resources/views/pages/answers.blade.php
+++ resources/views/pages/answers.blade.php
@@ -1,5 +1,27 @@
 <x-layouts.app>
     <h1>Question: {{ $question->question }}</h1>
+    @if ($question->open == 0)
+        <h2>Answers:</h2>
+        <ul>
+            @forelse ($question->answers as $answer)
+                @php
+                    $option = $answer->option;  // Assuming $answer->option_id links to an Option model
+                @endphp
+                <li>
+                    {{ $option->option }}
+                    @if ($option->correct == 1)
+                        <strong>(Correct)</strong>
+                    @elseif ($option->correct == 0)
+                        <strong>(Wrong)</strong>
+                    @endif
+                    - ({{ $answer->created_at->toFormattedDateString() }})
+                </li>
+
+            @empty
+                <p>No answers yet.</p>
+            @endforelse
+        </ul>
+    @else
         <h2>Answers:</h2>
         <ul>
             @forelse ($question->answers as $answer)
@@ -8,4 +30,5 @@
                 <p>No answers yet.</p>
             @endforelse
         </ul>
+    @endif
 </x-layouts.app>
