62cdd9573bcb7ce951cc8d14f884c9aec081948d
slavkoleminh <slavko007997@gmail.com>
2024-05-18 03:31:06 +0200

    UI answer-question

--- resources/views/livewire/answer.blade.php
+++ resources/views/livewire/answer.blade.php
@@ -1,6 +1,19 @@
-<div>
-    <form wire:submit="submitAnswer">
-        <input wire:model="answer" type="text" maxlength="255" placeholder="Enter answer">
-        <button type="submit">Save</button>
+<div class="max-w-md mx-auto bg-white rounded-lg shadow-lg">
+    <form wire:submit.prevent="submitAnswer" class="space-y-4">
+        <input
+            wire:model="answer"
+            type="text"
+            maxlength="255"
+            placeholder="Enter answer"
+            class="text-black w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-gray-800"
+        >
+        <button
+            type="submit"
+            class="w-full px-4 py-2 bg-gray-800 text-white rounded-md hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-gray-800"
+        >
+            Save
+        </button>
     </form>
 </div>
+
+
