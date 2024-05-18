e7662fdbcd704e00b2840d24503706e99fc5a06f
slavkoleminh <slavko007997@gmail.com>
2024-05-18 04:30:15 +0200

    delete button for options fixed

--- resources/views/livewire/question-option.blade.php
+++ resources/views/livewire/question-option.blade.php
@@ -11,7 +11,7 @@
         </div>
     </div>
 
-    <button type="submit" class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700 ml-2">
+    <button type="button" wire:click="remove" class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700 ml-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
              stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round"
