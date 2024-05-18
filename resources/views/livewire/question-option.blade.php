6b14448e39843469d7bb0a9c0eea130706abb400
Martin Klacik <klacikmartin@protonmail.com>
2024-05-18 03:35:34 +0200

    checkbox fix

--- resources/views/livewire/question-option.blade.php
+++ resources/views/livewire/question-option.blade.php
@@ -6,8 +6,8 @@
 
 
     <div class="flex items-center mb-4">
-        <input type="checkbox" wire:change="updateValue" id="correct" wire:model="correct" value="1" class="mr-2">
-        <label for="correct" class="text-sm font-medium text-gray-900 dark:text-white">{{ __('message.correct') }}</label>
+        <input type="checkbox" wire:change="updateValue" id="correct-{{ $id }}" wire:model="correct" value="1" class="mr-2">
+        <label for="correct-{{ $id }}" class="text-sm font-medium text-gray-900 dark:text-white">{{ __('message.correct') }}</label>
     </div>
 
     <button type="button" wire:click="remove" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{ __('message.remove') }}</button>
