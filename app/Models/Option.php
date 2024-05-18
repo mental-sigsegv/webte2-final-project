783efe9af769a23267bb2b3b5352cdef9ad32214
Martin Klacik <klacikmartin@protonmail.com>
2024-05-18 16:50:07 +0200

    testing seeders

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'question_code',
         'option',
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'question',
         'code',
